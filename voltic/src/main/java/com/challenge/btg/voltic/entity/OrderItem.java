a21ae079a77bf45a19be8d5cff55fc9d8a639d8c
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-15 10:44:29 -0300

    feat: Entity mapping

---
+++ voltic/src/main/java/com/challenge/btg/voltic/entity/OrderItem.java
@@ -0,0 +1,43 @@
+package com.challenge.btg.voltic.entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private String productName;
+    private Integer quantity;
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+
+    //--------------------------------------------//
+    public OrderItem() {
+
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
