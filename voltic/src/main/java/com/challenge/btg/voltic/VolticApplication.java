6a6accfe8d31e12e232344f4a0ffaa3def9b5417
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-15 10:04:47 -0300

    Initial commit

---
+++ voltic/src/main/java/com/challenge/btg/voltic/VolticApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.btg.voltic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolticApplication.class, args);
+	}
+
+}
---
+++ voltic/src/test/java/com/challenge/btg/voltic/VolticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.btg.voltic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
